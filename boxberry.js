b4e5899387f8d35b526c1e462cff01be063d350f
lilyok <lilyoknabieva@gmail.com>
2021-11-15 11:40:18 +0000

    add boxberry and pochta

---
+++ boxberry.js
@@ -0,0 +1,40 @@
+function getBoxBerryWidget(key, pointName="Доставка Boxberry", addressInputName="tildadelivery-onelineaddress",
+						   startCityName="Уфа", weight=4000, height=35, width=35, depth=20, addressWrapper="addresses-wrapper") {
+    function callbackBB_function(data) {
+        var dcash = parseFloat(data.price);
+        window.tcart.delivery.price = dcash;
+        var addr = data.name + ": " + data.address;
+        $('.t706__cartwin-totalamount-info_value').each(
+            function(i) {
+                if (i==1) this.innerHTML = dcash + ' р.';
+                i++
+        });
+        $("input[name='tildadelivery-onelineaddress']").val(addr);
+        window.tcart.amount = window.tcart.prodamount + dcash;
+        $('.t706__cartwin-totalamount').html(window.tcart.amount  + ' р.');
+    }
+
+    $("input[name='tildadelivery-onelineaddress']").prop("readonly", true);
+    
+    var $div = $("<div>", {id: "boxberry_map",
+        "style": "width: 100%;height: 500px"});
+        
+    $('.t706__cartwin-totalamount-info').append(
+      '<span class="t706__cartwin-totalamount-info_label">' + pointName + ':</span>' +
+      '<span class="t706__cartwin-totalamount-info_value">? p.</span>'
+    );
+
+    $( "#" + addressWrapper ).prepend($div);
+    boxberry.openOnPage('boxberry_map');
+    boxberry.open(
+        callbackBB_function,
+        key,
+        startCityName,
+        "",
+        window.tcart.prodamount,  // order price for insurance
+        weight,
+        0,                        // order was paid in advance
+        height, width, depth
+    );
+    return false;
+}
---
+++ pochta.js
@@ -0,0 +1,40 @@
+// Почта России
+function getPochtaWidget(pointName="Отделения и почтоматы", addressInputName="tildadelivery-onelineaddress",
+                        addressWrapper="addresses-wrapper"){
+    $("input[name='" + addressInputName + "']").prop("readonly", true);
+    var all = $(".t-input-description").map(function() {
+        return this.innerHTML;
+    }).get();
+    var start_location = all.join('');
+
+    var $div = $("<div>", {id: "ecom-widget",
+        "style": "width: 100%;height: 500px"});
+        
+    $('.t706__cartwin-totalamount-info').append(
+      '<span class="t706__cartwin-totalamount-info_label">' + pointName + ':</span>' +
+      '<span class="t706__cartwin-totalamount-info_value">? p.</span>'
+    );
+    funcStr = "function callbackPostCode(data) {"+
+        "  var dcash = data.cashOfDelivery / 100;" +
+        "  window.tcart.delivery.price = dcash;" +
+        "  var addr = data.regionTo + ', ' + data.cityTo + ', ' + data.addressTo + ', ' + data.pvzType;" +
+        "  $('.t706__cartwin-totalamount-info_value').each(" + 
+        "    function(i) {" +
+               "if (i==1) this.innerHTML = dcash + ' р.'; i++" +
+        "    });"+
+        "  window.tcart.amount = window.tcart.prodamount + dcash;" +
+        "  $('.t706__cartwin-totalamount').html(" +
+                "window.tcart.amount  + ' р.');" +
+        "  $(\"input[name='" + addressInputName + "']\").val(" +
+        "    addr);" +
+        "}";
+    str = "<script>ecomStartWidget({" +
+        "id: 19205," + 
+        "callbackFunction: callbackPostCode," +
+        "containerId: 'ecom-widget'," + 
+        "start_location: '" + start_location + "'," + 
+        "});" + funcStr + "<"+  "/script>";
+
+    $div.append(str);
+    $( "#addresses-wrapper" ).prepend($div);
+}
